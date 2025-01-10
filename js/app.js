74a59273d3d284c13b791de737040d0ead90f704
HosseinSoleimani <mrhosseinsoleimani@gmail.com>
2025-01-10 14:07:52 +0330

    todo cods dynamiced

--- js/app.js
+++ js/app.js
@@ -1,52 +1,77 @@
-const taskInput=document.getElementById("task-input");
-const dateInput=document.getElementById("date-input");
-const addButton=document.getElementById("add-button");
-const alertMessage= document.getElementById("alert-message");
+const taskInput = document.getElementById("task-input");
+const dateInput = document.getElementById("date-input");
+const addButton = document.getElementById("add-button");
+const alertMessage = document.getElementById("alert-message");
+const todoBody = document.querySelector("tbody");
 
-const todos=JSON.parse(localStorage.getItem("todos")) || [];
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
 
+const generateId = () => {
+  return Math.round(
+    Math.random() * Math.random() * Math.pow(10, 15)
+  ).toString();
+};
 
-const generateId = ()=>{
-   return Math.round(Math.random()*Math.random()*Math.pow(10,15)).toString();
-}
+const showAlert = (message, type) => {
+  alertMessage.innerHTML = "";
+  const alert = document.createElement("p");
+  alert.innerHTML = message;
+  alert.classList.add("alert");
+  alert.classList.add(`alert-${type}`);
+  alertMessage.append(alert);
 
-const showAlert = (message,type)=>{
-alertMessage.innerHTML="";
-const alert = document.createElement("p");
-alert.innerHTML= message;
-alert.classList.add("alert");
-alert.classList.add(`alert-${type}`);
-alertMessage.append(alert);
+  setTimeout(() => {
+    alert.style.display = "none";
+  }, 2000);
+};
 
-setTimeout(() => {
-    alert.style.display="none";
-}, 2000);
-}
+const saveToLocalStorage = () => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
 
-const saveToLocalStorage = ()=>{
-    localStorage.setItem("todos", JSON.stringify(todos))
-}
+const displayTodo = () => {
+  todoBody.innerHTML = "";
+  if (!todos.length) {
+    todoBody.innerHTML = "<tr><td colspan='4'>No task found!</td></tr>";
+    return;
+  }
 
+  todos.forEach((todo) => {
+    todoBody.innerHTML += `<tr>
+    <td>${todo.task}</td>
+    <td>${todo.date || "No date"}</td>
+    <td>${todo.completed ? "completed" : "pending"}</td>
+    <td>
+    <button>Edite</button>   
+    <button>Do</button>
+    <button>Delete</button>
+    </td>
+    </tr>`;
+  });
+};
 
-const addHandler=()=>{
-    const task=taskInput.value;
-    const date=dateInput.value;
-    const todo={
-     id:generateId(),
-     completed:false,
-     task,
-     date,
-}
-console.log(todos);
-if(task){
+displayTodo();
+
+const addHandler = () => {
+  const task = taskInput.value;
+  const date = dateInput.value;
+  const todo = {
+    id: generateId(),
+    completed: false,
+    task,
+    date,
+  };
+
+  if (task) {
     todos.push(todo);
     saveToLocalStorage();
-    taskInput.value="";
-    dateInput.value="";
-    showAlert("todo added successfully","success");
-}else{
-    showAlert("please enter a todo","error");
-}
-}
+    displayTodo();
+    taskInput.value = "";
+    dateInput.value = "";
+    showAlert("todo added successfully", "success");
+  } else {
+    showAlert("please enter a todo", "error");
+  }
+};
 
-addButton.addEventListener("click",addHandler);
+addButton.addEventListener("click", addHandler);
